2c92c482902a17a122ec63a9f0bbcab62c7e5fc7
John Wallace <John.Wallace@noaa.gov>
2019-05-03 15:35:27 -0700

    Create nameConvertVdrfdToCompFT.R

---
+++ nameConvertVdrfdToCompFT.R
@@ -0,0 +1,35 @@
+
+nameConvertVdrfdToCompFT <- JRWToolBox::scanIn("
+
+                      Comp_FT                                vdrfd
+                   COUNCIL_CODE                             COUNCIL
+                   AGENCY_CODE                              AGID
+                   LANDING_YEAR                             YEAR
+                   LANDING_DATE                             TDATE
+                   PACFIN_SPECIES_CODE                      SPID
+                   PARTICIPATION_GROUP_CODE                 PARGRP
+                   PACFIN_PORT_CODE                         PCID
+                   PACFIN_CATCH_AREA_CODE                   ARID
+                   PORT_CODE                                PORT
+                   FLEET_CODE                               FLEET
+                   VESSEL_NUM                               DRVID
+                   PACFIN_GEAR_CODE                         GRID
+                   IS_IFQ_LANDING                           IFQ_LANDING
+                   REMOVAL_TYPE_CODE                        REMOVAL_TYPE
+                   CONDITION_CODE                           COND
+                   DISPOSITION_CODE                         DISP
+                   EXVESSEL_REVENUE                         REV
+                   GRADE_CODE                               GRADE                 
+                   ROUND_WEIGHT_LBS                         CATCH.LBS
+                   LANDED_WEIGHT_LBS                        LWT_LBS
+                   ADJUSTED_GEAR_CODE                       ADJ_GRID
+                   DAHL_GROUNDFISH_CODE                     DAHL_SECTOR
+                   IS_REMOVAL_LEGAL                         LEGAL_REMOVAL
+                   IS_OVERAGE                               OVERAGE
+                   PRODUCT_FORM_CODE                        PRODUCT_FROM
+                   PRODUCT_USE_CODE                         PRODUCT_USE
+                   ORIG_PACFIN_CATCH_AREA_CODE              FTL_ARID
+                   ORIG_PACFIN_SPECIES_CODE                 FTL_SPID
+                   DEALER_NUM                               PROC
+                  
+")
