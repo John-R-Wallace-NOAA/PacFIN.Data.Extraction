738ebfd8c122f4797c52aadedbecb8d98edb6acd
John Wallace <John.Wallace@noaa.gov>
2020-07-24 18:48:25 -0700

    Update Main BDS Extraction Script, Copper rockfish example.R

--- Main BDS Extraction Script, Copper rockfish example.R
+++ Main BDS Extraction Script, Copper rockfish example.R
@@ -1,7 +1,7 @@
 
    library(JRWToolBox)
 
-   rgit::gitAFile('John-R-Wallace-NOAA/PacFIN-Data-Extraction/master/BDS Sp Extraction with Grade & Dahl Info 24 Jul 2020.R', show = FALSE)
+   rgit::gitAFile('John-R-Wallace-NOAA/PacFIN-Data-Extraction/master/R/BDS Sp Extraction with Grade & Dahl Info 24 Jul 2020.R', show = FALSE)
         
    PacFIN.COPP.bds.24.Jul.2020 <- bds.sp.extraction("'COPP'", PacFIN.Catch.Dahl = COPP.CompFT.24.Jul.2020)
       
