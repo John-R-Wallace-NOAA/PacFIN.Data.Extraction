e87fd44418da6000744f8df63f48b3d8fcb6a285
John Wallace <John.Wallace@noaa.gov>
2020-07-27 01:12:35 -0700

    rgit::S() approach

--- Main BDS Extraction Script, Copper rockfish example.R
+++ Main BDS Extraction Script, Copper rockfish example.R
@@ -1,8 +1,13 @@
 
    library(JRWToolBox)  # rgit package now loaded with JRWToolBox
 
-   rgit::gitAFile('John-R-Wallace-NOAA/PacFIN-Data-Extraction/master/R/BDS Sp Extraction with Grade & Dahl Info 24 Jul 2020.R', show = FALSE)
-        
+   # Use gitAFile() directly
+   # rgit::gitAFile('John-R-Wallace-NOAA/PacFIN-Data-Extraction/master/R/BDS Sp Extraction with Grade & Dahl Info 24 Jul 2020.R', show = FALSE)        
+   
+   # Use S() which calls gitAFile() 
+   repoPath <- "John-R-Wallace-NOAA/PacFIN-Data-Extraction"
+   rgit::S('BDS Sp Extraction with Grade & Dahl Info 24 Jul 2020.R', show = FALSE)
+
    PacFIN.COPP.bds.24.Jul.2020 <- bds.sp.extraction("'COPP'", PacFIN.Catch.Dahl = COPP.CompFT.24.Jul.2020)
       
    save(PacFIN.COPP.bds.24.Jul.2020, file = 'PacFIN.COPP.bds.24.Jul.2020.RData')
