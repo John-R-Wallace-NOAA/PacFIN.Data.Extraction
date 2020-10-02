968ec72765d23e35d9e1bd17a5330060733d54de
John Wallace <John.Wallace@noaa.gov>
2020-10-02 12:49:54 -0700

    Create Convert.PacFIN.ComprehensiveFT.ColNames.to.Legacy.Names.R

---
+++ R/Convert.PacFIN.ComprehensiveFT.ColNames.to.Legacy.Names.R
@@ -0,0 +1,75 @@
+
+Convert.PacFIN.ComprehensiveFT.ColNames.to.Legacy.Names <- function(CompFT) {
+
+  ' # --- Functions from John Wallace's toolbox: https://github.com/John-R-Wallace-NOAA/JRWToolBox ---- ' 
+ 
+  scanIn <- function (text, header = TRUE, ncol = 2, matrix = TRUE, numeric = FALSE) {
+  
+      if (matrix) {
+          Out <- data.frame(matrix(scan(what = "", text = text, 
+              quiet = TRUE), ncol = ncol, byrow = TRUE), stringsAsFactors = FALSE)
+          if (header) {
+              names(Out) <- as.character(Out[1, ])
+              Out <- Out[-1, ]
+          }
+          Out <- JRWToolBox::renum(Out)
+      }
+      else {
+          if (numeric) 
+              Out <- scan(text = text, quiet = TRUE)
+          else Out <- scan(what = "", text = text, quiet = TRUE)
+      }
+      Out
+  }
+  
+
+  recode.simple <- function (Codes, Table)   {
+  
+      toChange <- Codes %in% Table[, 1]
+      Codes[toChange] <- Table[, 2][match(Codes[toChange], Table[, 
+          1])]
+      Codes
+  }
+   
+  ' # End functions'
+  
+  nameConvertVdrfdToCompFT <- scanIn("
+                        Comp_FT                                vdrfd
+                     COUNCIL_CODE                             COUNCIL
+                     AGENCY_CODE                              AGID
+                     LANDING_YEAR                             YEAR
+                     LANDING_DATE                             TDATE
+                     PACFIN_SPECIES_CODE                      SPID
+                     PARTICIPATION_GROUP_CODE                 PARGRP
+                     PACFIN_PORT_CODE                         PCID
+                     PACFIN_CATCH_AREA_CODE                   ARID
+                     PORT_CODE                                PORT
+                     FLEET_CODE                               FLEET
+                     VESSEL_NUM                               DRVID
+                     PACFIN_GEAR_CODE                         GRID
+                     PACFIN_GROUP_GEAR_CODE                   GRGROUP
+                     IS_IFQ_LANDING                           IFQ_LANDING
+                     REMOVAL_TYPE_CODE                        REMOVAL_TYPE
+                     CONDITION_CODE                           COND
+                     DISPOSITION_CODE                         DISP
+                     EXVESSEL_REVENUE                         REV
+                     GRADE_CODE                               GRADE                 
+                     ROUND_WEIGHT_LBS                         CATCH.LBS
+                     LANDED_WEIGHT_LBS                        LWT_LBS
+                     ADJUSTED_GEAR_CODE                       ADJ_GRID
+                     DAHL_GROUNDFISH_CODE                     DAHL_SECTOR
+                     IS_REMOVAL_LEGAL                         LEGAL_REMOVAL
+                     IS_OVERAGE                               OVERAGE
+                     PRODUCT_FORM_CODE                        PRODUCT_FROM
+                     PRODUCT_USE_CODE                         PRODUCT_USE
+                     ORIG_PACFIN_CATCH_AREA_CODE              FTL_ARID
+                     ORIG_PACFIN_SPECIES_CODE                 FTL_SPID
+                     DEALER_NUM                               PROC
+                    
+  ")
+  
+  recode.simple(names(CompFT), nameConvertVdrfdToCompFT)
+   
+ }  
+   
+   
