1314479ce8f79174f74f2a585a9b15d208fa714d
John Wallace <John.Wallace@noaa.gov>
2021-01-25 13:25:30 -0800

    Switched to httr based sourceFunctionURL()

--- R/nameConvertVdrfdToCompBDS.R
+++ R/nameConvertVdrfdToCompBDS.R
@@ -2,14 +2,16 @@
 
 # -------- Import utility Functions --------
 
-sourceFunctionURL <- function(URL) {
-       ' # For more functionality, see gitAFile() in the rgit package ( https://github.com/John-R-Wallace-NOAA/rgit ) which includes gitPush() and git() '
-       require(RCurl)
-       File.ASCII <- tempfile()
-       on.exit(file.remove(File.ASCII))
-       writeLines(paste(readLines(textConnection(RCurl::getURL(URL))), collapse = "\n"), File.ASCII)
-       source(File.ASCII, local = parent.env(environment()))
+sourceFunctionURL <- function (URL) {
+    " # For more functionality, see gitAFile() in the rgit package ( https://github.com/John-R-Wallace-NOAA/rgit ) which includes gitPush() and git() "
+    require(httr)
+    File.ASCII <- tempfile()
+    on.exit(file.remove(File.ASCII))
+    getTMP <- httr::GET(URL)
+    write(paste(readLines(textConnection(httr::content(getTMP))), collapse = "\n"), File.ASCII)
+    source(File.ASCII, local = parent.env(environment()))
 }
+    
 
 sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/scanIn.R")
 
